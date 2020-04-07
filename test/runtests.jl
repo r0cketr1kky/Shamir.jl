1c7e38d7a849cfe0c63288e4b06e269d960f8031
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-04-07 14:58:13 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,9 @@ using Shamir, Polynomials, Test
 
 @info "Testing creation of shares"
 prod_coeffs = [1234, 166, 94]
+n = 6
 prod_poly = Poly(prod_coeffs)
-shares = Shamir.construct_shares(prod_poly)
+shares = Shamir.construct_shares(n, prod_poly)
 @test shares == [[1, 1494] [2, 1942] [3, 2578] [4, 3402] [5, 4414] [6, 5614]]
 
 
