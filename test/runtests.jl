26177905bba33e4c472a2409ca195493e267aed9
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-04-07 14:39:58 +0530

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,17 @@
+using Shamir, Polynomials, Test
+
+@info "Testing creation of shares"
+prod_coeffs = [1234, 166, 94]
+prod_poly = Poly(prod_coeffs)
+shares = Shamir.construct_shares(prod_poly)
+@test shares == [[1, 1494] [2, 1942] [3, 2578] [4, 3402] [5, 4414] [6, 5614]]
+
+
+@info "Testing Recover secret"
+prod_coeffs = [1234, 166, 94]
+prod_poly = Poly(prod_coeffs)
+shares = Shamir.construct_shares(prod_poly)
+secret = Shamir.recover_secret(shares)
+@test secret == 1234.0
+
+@info "All tests completed"
