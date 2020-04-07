55ccee6e6c22b36fd17a4c59ad05809f0019e3d7
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-04-07 15:00:28 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ prod_coeffs = [1234, 166, 94]
 n = 6
 prod_poly = Poly(prod_coeffs)
 shares = Shamir.construct_shares(n, prod_poly)
-@test shares == [[1, 1494] [2, 1942] [3, 2578] [4, 3402] [5, 4414] [6, 5614]]
+@test shares == [[1, 1494], [2, 1942], [3, 2578], [4, 3402], [5, 4414], [6, 5614]]
 
 
 @info "Testing Recover secret"
